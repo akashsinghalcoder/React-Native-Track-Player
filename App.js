f1f4b3f3e11c82ca99f0e2a1e6346dd194ed14ca
Akash Singhal <akashsinghal@Akashs-MacBook-Pro.local>
2018-09-26 10:00:25 +0530

    basic music player

---
+++ App.js
@@ -0,0 +1,104 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View,Button} from 'react-native';
+import TrackPlayer from 'react-native-track-player';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props)
+  {
+    super(props);
+     this.getstate = this.getstate.bind(this);
+  }
+
+
+
+
+
+
+
+
+
+getstate()
+ {
+  
+  TrackPlayer.setupPlayer().then(async () => {
+
+
+    TrackPlayer.updateOptions({
+            capabilities: [
+                TrackPlayer.CAPABILITY_PLAY,
+                TrackPlayer.CAPABILITY_PAUSE,
+                TrackPlayer.CAPABILITY_SEEK_TO,
+                TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+                TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS
+            ]
+        });
+ 
+    // Adds a track to the queue
+    await TrackPlayer.add({
+        id: 'trackId',
+        url: require('./audio_house.mp3'),
+        title: 'Track Title',
+        artist: 'Track Artist',
+        
+    });
+ 
+    // Starts playing it
+    TrackPlayer.play();
+ 
+});
+
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+        <Button
+  onPress={this.getstate}
+  title="Learn More"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ android/app/src/main/java/com/testingapplicationn/MainActivity.java
@@ -0,0 +1,16 @@
+package com.testingapplicationn;
+
+import com.facebook.react.ReactActivity;
+import guichaguri.trackplayer.TrackPlayer;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "testingapplicationn";
+    }
+}
